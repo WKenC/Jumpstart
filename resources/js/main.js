5805042fd32de605e1d681664cc380859a21e6f5
wingMan <33496072+WKenC@users.noreply.github.com>
2018-02-01 17:34:00 -0800

    Added all complete; slick downloaed local, CDN not working

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,26 @@
-This is a new js file.
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 500,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  var howfarScrolled = 0;
+  $(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - howfarScrolled > 50){
+      var navheight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navheight}, 150);
+      howfarScrolled = scrollTop;
+    } else if (howfarScrolled - scrollTop > 50){
+      $('.navbar').animate({top: '0px'}, 150);
+      howfarScrolled = scrollTop;
+    }
+  });
+  
+});
